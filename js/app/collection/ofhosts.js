5fb3809ef0aa8fe1958d9e9bf32adb2d1dfeba69
Tomas Doran <bobtfish@bobtfish.net>
2012-01-29 15:01:42 +0000

    Actually use the per host classes

--- js/app/collection/ofhosts.js
+++ js/app/collection/ofhosts.js
@@ -6,20 +6,13 @@ var CollectionOfHosts = Backbone.Collection.extend({
   },
   parse: function(response) {
       var collection = this;
-      var rows = response.rows;
-      $.each(rows, function(index, value) { /* This logic should be in the model class we inflate */
+      var rows = [];
+      $.each(response.rows, function(index, value) { /* This logic should be in the model class we inflate */
           var id = value["_id"]["$oid"];
           value["id"] = id;
           delete value["_id"];
-          delete value["agentlist"];
-          delete value["extra"];
-          value["fqdn_sortable"] = value["facts"]["fqdn"].split('.').reverse().join('.');
-          $.each(["ps", "kernel", "title", "facterversion", "hardwareisa", "rubysitedir", "rubyversion", "lsbrelease",
-                "module_name", "selinux", "type", "caller_module_name", "never", "files", "servername", "id", "puppetversion"],
-            function(i, name) { delete value["facts"][name] });
-          var lastseen = new Date(1000*value["lastseen"]);
-          value["lastseen_string"] = lastseen.toString();
-          $.each(value["classes"], function (i, name) { collection.PuppetClasses.add_unless_exists(name) });
+          value["_PuppetClasses"] = collection.PuppetClasses;
+          rows.push(new Host(value));
       });
       return rows;
   },
--- js/app/model/host.js
+++ js/app/model/host.js
@@ -1,2 +1,16 @@
-var Host = Backbone.Model.extend({
+window.Host = Backbone.Model.extend({
+    initialize: function () {
+        delete this.attributes["agentlist"];
+        delete this.attributes["extra"];
+        this.attributes["fqdn_sortable"] = this.attributes["facts"]["fqdn"].split('.').reverse().join('.');
+        var me = this;
+        $.each(["ps", "kernel", "title", "facterversion", "hardwareisa", "rubysitedir", "rubyversion", "lsbrelease",
+                "module_name", "selinux", "type", "caller_module_name", "never", "files", "servername", "id", "puppetversion"],
+            function(i, name) { delete me.attributes["facts"][name] });
+        var lastseen = new Date(1000*this.attributes["lastseen"]);
+        this.attributes["lastseen_string"] = lastseen.toString();
+        var PuppetClasses = this.attributes["_PuppetClasses"];
+        delete this.attributes["_PuppetClasses"];
+        $.each(this.attributes["classes"], function (i, name) { PuppetClasses.add_unless_exists(name) });
+    }
 });
