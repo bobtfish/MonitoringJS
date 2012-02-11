0fd6b9f3519f23dacfde718eefe5268d040d1828
Tomas Doran <bobtfish@bobtfish.net>
2012-02-11 17:49:40 +0000

    Get the nagios results to be decent

--- js/app/collection/nagiosresults.js
+++ js/app/collection/nagiosresults.js
@@ -2,6 +2,9 @@ var CollectionOfNagiosResults = Backbone.Collection.extend({
   model: NagiosServiceResult,
   isOk: function () {
       return this.all(function(val){ return val.isOk() });
+  },
+  comparator: function(ob) {
+      return ob.isOk() + ob.get("name");
   }
 });
 
--- js/app/model/nagiosserviceresult.js
+++ js/app/model/nagiosserviceresult.js
@@ -1,4 +1,10 @@
 var NagiosServiceResult = Backbone.Model.extend({
+    initialize: function () {
+        delete this.attributes["agentlist"];
+        var date = new Date( this.get("last_notification") *1000);
+        this.set({"last_notification":  date});
+        return this;
+    },
     isOk: function () {
         //alert("In isOk");
         if (this.get("current_state") == "0") {
