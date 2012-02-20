46c993634c588e6aa80b5eb530d54799cb7e15b1
Tomas Doran <bobtfish@bobtfish.net>
2012-02-20 20:18:18 +0000

    Make hippie reconnect in the face of even more network shonkyness

--- js/app/hippie.js
+++ js/app/hippie.js
@@ -4,10 +4,14 @@ var ourHippie = function (event_router) {
         var hippie = new Hippie( document.location.host, 5, function() {
                 event_router.trigger("hippie:connected", "ok");
                 console.log("Hippie: Connected");
+                object.connected = 1;
+                object.connecting_timeout = false;
             },
             function() {
                 event_router.trigger("hippie:disconnected", "ok");
                 console.log("Hippie: disconnected");
+                object.connected = false;
+                object.connecting_timeout = false;
                 object.hippie = false;
             },
             function(e) {
@@ -15,12 +19,25 @@ var ourHippie = function (event_router) {
                 //console.log("Hippie: got message: " + JSON.stringify(e));
             }
         );
-        this.hippie = hippie;
+        object.hippie = hippie;
+        object.connecting_timeout = setTimeout(function () {
+            alert("Connecting timeout");
+            if (!object.connected && object.connecting_timeout && !object.new_hippie_timeout) {
+                console.log("Hippie: Connection timed out, restarting connect");
+                object.connected = false;
+                object.new_hippie_timeout = false;
+                object.connecting_timeout = false;
+                object.hippie = false;
+                object.buildHippie();
+            }
+            else {
+                object.connecting_timeout = false;
+            }
+        }, 30000);
         return hippie;
     };
 
     event_router.on("hippie:disconnected", function() {
-
         object.new_hippie_timeout = setTimeout(function () {
             console.log("Hippie: Reconnect");
             object.new_hippie_timeout = false;
