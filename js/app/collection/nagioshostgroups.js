95481bb2fe05f4c774e45671ebcab97dddbf3201
Tomas Doran <bobtfish@bobtfish.net>
2012-02-24 17:23:33 +0000

    Remove all the silly groups

--- js/app/collection/nagioshostgroups.js
+++ js/app/collection/nagioshostgroups.js
@@ -3,12 +3,38 @@ var CollectionOfNagiosHostGroups = MyCollection.extend({
     parse: function(response) {
         var collection = this;
         var by_group = {};
+        // FIXME - This should be metadata, not hard coded here
+        var skip_groups = [
+            "eight-disk-h200-servers",
+            "four-disk-3ware-servers",
+            "has-3ware-raid",
+            "has-3ware-raid2",
+            "ixn-servers",
+            "ixn-storage-servers",
+            "mogile-ramdisk",
+            "no-nginx",
+            "no-var",
+            "twelve-disk-h200-servers",
+            "web-servers",
+            "eight-disk-3ware-jbod-servers",
+            "eight-disk-3ware-servers",
+            "eight-disk-h700-servers",
+            "four-disk-3ware-jbod-servers",
+            "four-disk-h700-servers",
+            "has-h700",
+            "has-h700-raid",
+            "has-h700-raid2",
+            "has-megaraid",
+            "two-disk-3ware-servers",
+        ];
         $.each(response.rows, function(index, value) { /* This logic is mostly duplicated from collectionofhosts FIXME */
-            if (!by_group[value.group_name]) {
-                by_group[value.group_name] = [value.fqdn]
-            }
-            else {
-                by_group[value.group_name].push(value.fqdn);
+            if (!_.any(skip_groups, function(this_group) { return this_group == value.group_name })) {
+                if (!by_group[value.group_name]) {
+                    by_group[value.group_name] = [value.fqdn]
+                }
+                else {
+                    by_group[value.group_name].push(value.fqdn);
+                }
             }
         });
         var rows = [];
